26530ea8d13a3ede1db5c72aa4c0050044df8255
Angel Nicolas Payeras <anpayeras@teco.com.ar>
2025-04-23 20:11:31 -0300

    add login

--- utils/supabase/middleware.ts
+++ utils/supabase/middleware.ts
@@ -42,8 +42,14 @@ export const updateSession = async (request: NextRequest) => {
     }
 
     if (request.nextUrl.pathname === "/" && itineraryId) {
-      return NextResponse.redirect(new URL(`/protected/${itineraryId.value}`, request.url));
+      return NextResponse.redirect(
+        new URL(`/protected/${itineraryId.value}`, request.url)
+      );
       // return NextResponse.redirect(new URL("/sign-in", request.url));
+    }
+
+    if (request.nextUrl.pathname === "/" && !itineraryId) {
+      return NextResponse.redirect(new URL("/sign-in", request.url));
     }
 
     return response;
